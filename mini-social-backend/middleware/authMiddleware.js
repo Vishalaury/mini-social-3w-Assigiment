57012d48a220bfe246dbb094966f7c1f6a8735ec
Vishal Maurya <mauryavishal9919@gmail.com>
2026-04-03 04:02:05 +0530

    final fix cloudinary + image display

--- mini-social-backend/middleware/authMiddleware.js
+++ mini-social-backend/middleware/authMiddleware.js
@@ -1,3 +1,21 @@
+// const jwt = require('jsonwebtoken');
+
+// module.exports = (req, res, next) => {
+//   const token = req.header("Authorization");
+
+//   if (!token) return res.status(401).json({ msg: "No token" });
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch {
+//     res.status(401).json({ msg: "Invalid token" });
+//   }
+// };
+
+
+
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
@@ -7,9 +25,11 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    req.user = decoded.user; // 🔥 YAHI FIX HAI
+
     next();
-  } catch {
+  } catch (err) {
     res.status(401).json({ msg: "Invalid token" });
   }
 };
