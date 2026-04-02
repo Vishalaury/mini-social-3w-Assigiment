170264fea6e4bcf98fb2d5ef0f09deaeb79bb72e
Vishal Maurya <mauryavishal9919@gmail.com>
2026-04-02 22:45:18 +0530

    updated API URL for deployment

--- mini-social-frontend/src/api/api.js
+++ mini-social-frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
+// 
 const API = axios.create({
-  // baseURL: "http://localhost:5000" 
-   baseURL: "http://localhost:5000/api" 
+  baseURL: "https://mini-social-3w-assigiment.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
